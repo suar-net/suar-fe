71133d7870a2f173ba0cc44cae12317593c22cce
yodh4 <cyrilus2004@gmail.com>
2025-08-05 12:10:03 +0700

    implement layout with Sidebar, RequestPanel, and ResponsePanel components

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import Sidebar from './components/Sidebar.vue'
+import RequestPanel from './components/RequestPanel.vue'
+import ResponsePanel from './components/ResponsePanel.vue'
+import { Button } from '@/components/ui/button'
+import { Menu } from 'lucide-vue-next'
+
+const isSidebarOpen = ref(true)
+</script>
+
 <template>
-  <h1 class="text-4xl">Vue Jobs</h1>
+  <div class="flex h-screen bg-background">
+    <Sidebar :class="{ hidden: !isSidebarOpen, 'lg:flex': true }" />
+    <div class="flex-1 flex flex-col">
+      <div class="lg:hidden p-2 border-b">
+        <Button variant="outline" @click="isSidebarOpen = !isSidebarOpen">
+          <Menu class="h-6 w-6" />
+        </Button>
+      </div>
+      <RequestPanel />
+      <ResponsePanel />
+    </div>
+  </div>
 </template>
